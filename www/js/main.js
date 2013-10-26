bb9598a048125f8a5b43caa7637d42a873e68f1a
David Cavazos <davido262@gmail.com>
2013-10-26 13:05:17 -0500

    Funcionalidad de seleccionar todas las filas de la tabla

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,34 @@
 function toggle_modal() {
-  el = document.getElementById("modal");
-  el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
+  var e = document.getElementById('modal');
+  e.style.visibility = e.style.visibility == 'visible' ? 'hidden' : 'visible';
+}
+
+function toggle_select_all(prefix, boton) {
+  var e = document.getElementById('select_all');
+  var num = 0;
+  var ex;
+  do {
+    num += 1;
+    ex = document.getElementById(prefix + num);
+    if (ex != null) {
+      ex.checked = e.checked;
+    }
+  } while (ex != null);
+  validar_selecciones(prefix, boton);
+}
+
+function validar_selecciones(prefix, boton) {
+  var num = 0;
+  var ex;
+  var activado = false;
+  do {
+    num += 1;
+    ex = document.getElementById(prefix + num);
+    if (ex != null && ex.checked) {
+      activado = true;
+      break;
+    }
+  } while (ex != null);
+
+  document.getElementById(boton).disabled = !activado;
 }
