e4a7fda84a538eb7c0b32781e00466c30567bea7
Stefhan Alejandro Rodríguez Mejía <stefhan.rodriguez.m@gmail>
2013-10-30 16:13:39 -0600

    dolor de cabeza con las url amigables ^^

--- ctl/BaseCtl.php
+++ ctl/BaseCtl.php
@@ -32,6 +32,40 @@ abstract class BaseCtl {
            $this->generarBody() .
            $this->generarFooter();
     echo $vst;
+    //phpinfo(INFO_MODULE);
+    $this->amigableUrl();
+  }
+
+  public function amigableUrl() {
+    $server = $_SERVER['SERVER_NAME'];       /** localhost **/
+    $script = $_SERVER['PHP_SELF'];          /** /user100-ex2/index.php **/
+    $variables = $_SERVER['QUERY_STRING'];   /** ctl ... **/
+    $delimitador = '/';
+
+    /** $_SESSION, session_name() y session_start() evitan un bucle infinita de redireccionamiento **/
+    $_SESSION['bucle'] = 0;
+    session_name('url_a');
+    session_start();
+    
+    if ( !empty($variables) && $_SERVER['REQUEST_METHOD']=='GET' && $_SESSION['bucle']!=1) { 
+    //if ( !empty($variables) && $_SERVER['REQUEST_METHOD']=='GET' ) { 
+      $variables = preg_replace('/\//', '', $variables); /** ctl=ciclos/  =  ctl=ciclos**/
+      $variables = preg_replace('/(=|&)/', $delimitador, $variables); /** ctl=ciclos&altas  =  ctl/ciclos/altas**/
+      $variables = preg_replace('/ctl(\/){0,1}/', '', $variables); /**ctl proboca conflicto de redireccionamiento (bucle)**/
+      $aVariables = explode('/', $variables); /** array([0]->ctl [1]->ciclos [2]->altas) **/
+
+      $variables = ''; 
+      for ($i=0; $i < count($aVariables); $i++) { 
+        $variables .= $delimitador . $aVariables[$i]; 
+      } 
+
+      $script = preg_replace('/\/index.php/', '', $script); 
+      $url = 'http://' . $server . $script . $variables; 
+      print_r($url); 
+      $_SESSION['bucle'] = 1;
+      header('location: ' . $url);
+      exit;
+    }
   }
 
   protected function campo($field, $value, $body) {
