7256037b3e887fae7fc90675be000d5ba7ab3148
Stefhan Alejandro Rodríguez Mejía <stefhan.rodriguez.m@gmail>
2013-11-03 23:35:12 -0600

    boton cursos dia clase

--- www/js/jquery.plugin.js
+++ www/js/jquery.plugin.js
@@ -5,35 +5,29 @@
  * Uso como parámetro la variable windows para ahorrar tiempo de búsqueda de objetos globales.
  * Uso del parámetro undefined para asegurar que la variable es no definida.
  */
-/*! Por recomendacion. Evita errores por el mal cierre de scripts */
-/*!
- * función anónima que nos asegura que el símbolo $ 
- * sea del objeto jQuery, en caso que este sea usado como objeto por otras bibliotecas.
- * Uso como parámetro la variable windows para ahorrar tiempo de búsqueda de objetos globales.
- * Uso del parámetro undefined para asegurar que la variable es no definida.
- */
 var numeroDeDiasFestivos=10;
 var numeroDeDiasFestivosM=0;
 (function( $, window, undefined ) {
-  var DIAS_MAXIMOS = 7;
+  var DIAS_MAXIMOS = 7,
+      DIAS_MAXIMOS_CLASE = 7;
   $.fn.agregaDiaFestivo = function(contenedor) {
-	var contenedorDiasFestivos = contenedor,
+  var contenedorDiasFestivos = contenedor,
         contenedorDiasFestivos;
 
     // Uso de encadenamiento
     return this.each(function() { 
       $boton = $(this); 
       $boton.on('click', function () { 
-		$("#guardar").removeAttr("disabled");
+    $("#guardar").removeAttr("disabled");
         if ( $('.content').size() < DIAS_MAXIMOS ) { 
           numeroDeDiasFestivos++;
       $( contenedorDiasFestivos ).append( 
             '<div id="div_festivos'+numeroDeDiasFestivos+'" class="content">'+ 
               '<input id="inicio_'
-			  +numeroDeDiasFestivos+
-			  '" type="text" placeholder="aaaa-mm-dd" onblur="validaFecha(\'inicio_'
-			  +numeroDeDiasFestivos+
-			  '\')"/>'+
+        +numeroDeDiasFestivos+
+        '" type="text" placeholder="aaaa-mm-dd" onblur="validaFecha(\'inicio_'
+        +numeroDeDiasFestivos+
+        '\')"/>'+
 
               '<textarea id="descripcion_'+numeroDeDiasFestivos+'" onblur="validaDescripcion(\'descripcion_'+numeroDeDiasFestivos+'\')"></textarea>'+ 
               '<button class="quitarFestivo">-</button>'+ 
@@ -53,10 +47,10 @@ var numeroDeDiasFestivosM=0;
           $( contenedor ).append( 
             '<div id="div_festivos'+numeroDeDiasFestivosM+'" class="content">'+ 
               '<input id="inicio_m_'
-			  +numeroDeDiasFestivosM+
-			  '" type="text" placeholder="aaaa-mm-dd" onblur="validaFecha(\'inicio_m_'
-			  +numeroDeDiasFestivosM+
-			  '\')"/>'+
+        +numeroDeDiasFestivosM+
+        '" type="text" placeholder="aaaa-mm-dd" onblur="validaFecha(\'inicio_m_'
+        +numeroDeDiasFestivosM+
+        '\')"/>'+
 
               '<textarea id="descripcion_m_'+numeroDeDiasFestivosM+'" onblur="validaDescripcion(\'descripcion_m_'+numeroDeDiasFestivosM+'\')"></textarea>'+ 
               '<button class="quitarFestivo">-</button>'+ 
@@ -68,5 +62,25 @@ var numeroDeDiasFestivosM=0;
       });
     });
   }
+  $.fn.agregaDiaDeClase = function(contenedor) {
+    var id;
+    // Uso de encadenamiento
+    return this.each(function() { 
+      $boton = $(this); 
+      $boton.on('click', function () { 
+      $("#guardar").removeAttr("disabled");
+        if ( $('.content').size() < DIAS_MAXIMOS_CLASE ) { 
+          numeroDeDiasFestivos++;
+          $( contenedor ).append( 
+            '<div id="cClase'+id+'" class="content">'+ 
+              '<input id="fechaId'+id+'" type="text" placeholder="aaaa-mm-dd" onblur="validaFecha(\'cClase'+id+'\')"/>'+
+              '<button class="quitarDia">-</button>'+ 
+            '</div>');  
+        }; 
+      }); 
+      $('body').on('click', '.quitarDia', function () { 
+        $(this).parent().remove(); 
+      }); 
+    }); 
+  } 
 })( jQuery, window ); 
-
