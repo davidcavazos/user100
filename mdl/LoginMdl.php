5889f776dfda92c51186f9052bf8bf0fb376e74c
noiarek <noiarek@gmail.com>
2013-11-06 14:02:30 -0600

    Nuevos archivos login

---
+++ mdl/LoginMdl.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('mdl/BaseMdl.php');
+
+class LoginMdl extends BaseMdl
+{
+	public function verificar($codigo, $password)
+	{
+		$datos = $this->datos("SELECT * FROM usuario WHERE codigo='".$codigo."'")[0];
+		if($datos['password']==$password)
+		{
+			header('Location: index.php?ctl=mis_cursos');
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
