59742395227fd9c74b4ce993e81af7838ae47879
David Cavazos <davido262@gmail.com>
2013-11-06 17:49:58 -0600

    LoginMdl merge

--- mdl/LoginMdl.php
+++ mdl/LoginMdl.php
@@ -1,26 +1,20 @@
 <?php
 
 require_once('mdl/BaseMdl.php');
-<<<<<<< HEAD
-
 class LoginMdl extends BaseMdl
 {
-	public function verificar($codigo, $password)
-	{
-		$datos = $this->datos("SELECT * FROM usuario WHERE codigo='".$codigo."'")[0];
-		if($datos['password']==$password)
-		{
-			header('Location: index.php?ctl=mis_cursos');
-		}
-		else
-		{
-			return false;
-		}
-	}
-}
-=======
-class LoginMdl extends BaseMdl {
+  public function verificar($codigo, $password)
+  {
+    $datos = $this->datos("SELECT * FROM usuario WHERE codigo='".$codigo."'")[0];
+    if($datos['password']==$password)
+    {
+      header('Location: index.php?ctl=mis_cursos');
+    }
+    else
+    {
+      return false;
+    }
+  }
 }
 
 ?>
->>>>>>> b541d415210c07e2b93943629802713784bde6f8
